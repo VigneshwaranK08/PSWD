26d026ff6599c543134ffa3e0da7015d28f6df84
VigneshwaranK08 <learningvig@gmail.com>
2025-08-14 19:28:18 +0530

    implemented encoder

--- Encryption/Encoding.py
+++ Encryption/Encoding.py
@@ -1,3 +1,10 @@
 from cryptography.fernet import Fernet
-import json
 
+def Encoder(password):
+    with open('key.key','rb') as keyfile:
+        key = keyfile.read()
+        key = Fernet(key)
+
+    EncryptedPassword = key.encrypt(password.encode())
+
+    return EncryptedPassword
