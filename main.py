92f09fe900aec784355d1d2a4ccecb90a9ab91ef
VigneshwaranK08 <learningvig@gmail.com>
2025-08-14 17:48:34 +0530

    added indent while writing , byte writing fix, removed nargs

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def addservice(args):
     
     jsondata.append({"name":args.name,"field":args.field,"password":args.password})
 
-    with open("Passwords.json",'wb') as jsonfile:
-        json.dump(jsondata,jsonfile)
+    with open("Passwords.json",'w') as jsonfile:
+        json.dump(jsondata,jsonfile,indent=4)
 
 AddService = SubParser.add_parser("add")
 AddService.add_argument("name")
 AddService.add_argument('-f','--field')
-AddService.add_argument('-p','--password',nargs=1)
+AddService.add_argument('-p','--password',type=str)
 AddService.set_defaults(func=addservice)
 
 args = Parser.parse_args()
