e3c92d6e1edbe494777973570b80623d9036b10e
VigneshwaranK08 <learningvig@gmail.com>
2025-08-14 19:53:22 +0530

    added abspath with os module

--- Encryption/Encoding.py
+++ Encryption/Encoding.py
@@ -1,7 +1,13 @@
 from cryptography.fernet import Fernet
+import os
+
+def abspath(filename):
+    filepath = os.path.dirname(__file__) # this return the abs path of this file i.e /home/vignesh/Documents/roadmap-sh/python/PasswordManager/Encryption
+    
+    return os.path.join(filepath,filename) # its like join the above path / filename , but make sure its cross-platform
 
 def Encoder(password):
-    with open('/home/vignesh/Documents/roadmap-sh/python/PasswordManager/Encryption/key.key','rb') as keyfile:
+    with open(abspath('key.key'),'rb') as keyfile:
         key = keyfile.read()
         key = Fernet(key)
 
