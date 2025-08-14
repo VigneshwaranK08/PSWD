49b3ac4c2aa509e799048db1382b13da3f11c018
VigneshwaranK08 <learningvig@gmail.com>
2025-08-14 21:32:45 +0530

    added Decoding process

---
+++ Encryption/Decoding.py
@@ -0,0 +1,19 @@
+import json
+from cryptography.fernet import Fernet
+import os
+
+def abspath(filename):
+    folderpath = os.path.dirname(__file__)
+    path = os.path.join(folderpath,filename)
+
+    return path
+
+def Decoder(password):
+
+    with open(abspath('key.key'),'rb') as keyfile:
+        key = keyfile.read()
+        key = Fernet(key) # str to byte
+
+    DecryptedPassword = key.decrypt(password) 
+    
+    return DecryptedPassword
