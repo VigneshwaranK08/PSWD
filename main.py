110971f27cf20985e98a9ca92445564ba9a629d9
VigneshwaranK08 <learningvig@gmail.com>
2025-08-14 15:47:28 +0530

    fix

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-subparser = parser.add_subparsers()
+Parser = argparse.ArgumentParser()
+SubParser = Parser.add_subparsers()
 
 def addservice(args):
     print("added password",args)
 
-addservice = subparser.add_parser("add")
-addservice.add_argument("name")
-addservice.add_argument('-f','--field')
-addservice.add_argument('-p','--password',nargs=1)
-addservice.set_defaults(func=addservice)
+AddService = SubParser.add_parser("add")
+AddService.add_argument("name")
+AddService.add_argument('-f','--field')
+AddService.add_argument('-p','--password',nargs=1)
+AddService.set_defaults(func=addservice)
 
-args = parser.parse_args()
+args = Parser.parse_args()
 args.func(args)
