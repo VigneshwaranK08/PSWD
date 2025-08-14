7052716d4de13eb8d8571b021de5327ea69db13b
VigneshwaranK08 <learningvig@gmail.com>
2025-08-14 17:42:50 +0530

    implemented writing data to json file

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import argparse
+import json
 
 Parser = argparse.ArgumentParser()
 SubParser = Parser.add_subparsers()
 
 def addservice(args):
-    print("added password",args)
+    try:
+        with open("Passwords.json",'rb') as jsonfile:
+            jsondata = json.load(jsonfile)
+    except:
+        jsondata = []
+    
+    jsondata.append({"name":args.name,"field":args.field,"password":args.password})
+
+    with open("Passwords.json",'wb') as jsonfile:
+        json.dump(jsondata,jsonfile)
 
 AddService = SubParser.add_parser("add")
 AddService.add_argument("name")
